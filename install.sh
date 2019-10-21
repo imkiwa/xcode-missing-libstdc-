47605b007bc866dc6127043ce04d106366210060
imkiva <libkernelpanic@gmail.com>
2019-10-21 14:45:35 +0800

    libstdc++: init

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo ln -s $PWD/include/c++ /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+
+sudo ln -s $PWD/lib/libstdc++.6.0.9.tbd $PWD/lib/libstdc++.6.tbd $PWD/lib/libstdc++.tbd /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/
