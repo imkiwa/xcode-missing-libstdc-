93e8d061ab96af93d1aa124520db12d0a700a978
imkiva <libkernelpanic@gmail.com>
2019-11-11 20:46:45 +0800

    shell: improve install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
-sudo ln -s $PWD/include/c++ /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+#!/usr/bin/env bash
 
-sudo ln -s $PWD/lib/libstdc++.6.0.9.tbd $PWD/lib/libstdc++.6.tbd $PWD/lib/libstdc++.tbd /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/
+set -e
+
+sudo ln -s \
+    $PWD/include/c++ \
+    /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+
+sudo ln -s \
+    $PWD/lib/libstdc++.6.0.9.tbd \
+    $PWD/lib/libstdc++.6.tbd \
+    $PWD/lib/libstdc++.tbd \
+    /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/
